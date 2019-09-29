40e9e6f5d0a309fd5b0ce594c4be033527ebebc6
Dennis Collective <dennis.collective@gmail.com>
2019-09-29 04:10:18 -0700

    use applicative style rather than monadic

--- app/Main.hs
+++ app/Main.hs
@@ -8,15 +8,16 @@ import           Network.Wreq
 import           System.Environment
 import           Text.CSV
 
+main :: IO ()
+main = csv >>= print
+  where
+    url = errorIfBlank <$> (lookupEnv "SHEET_URL")
+    response = rBody <$> (url >>= get)
+    csv = parseCSV <$> url <*> response
 
 errorIfBlank :: Maybe String -> String
 errorIfBlank Nothing  = error "set env SHEET_URL"
 errorIfBlank (Just s) = s
 
-main :: IO ()
-main = do
-  url <- lookupEnv "SHEET_URL"
-  r <- get (errorIfBlank url)
-  parsed <-
-    return (parseCSV (errorIfBlank url) (Char8.unpack $ r ^. responseBody))
-  print $ parsed
+rBody :: (Response ByteString) -> [Char]
+rBody r = Char8.unpack $ r ^. responseBody
