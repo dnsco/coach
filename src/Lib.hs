12a85bdae3960d4558267382b1017ebe2c7c8583
Dennis Collective <dennis.collective@gmail.com>
2019-09-30 09:53:27 -0700

    refactor to move generators next to each other

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,22 +5,13 @@ module Lib
 
 import           Control.Lens
 import qualified Data.ByteString.Lazy.Char8 as Char8 (ByteString, unpack)
-import           Data.Maybe                 (catMaybes)
 import           Network.Wreq
 import           Text.CSV
 import           Text.Parsec.Error          (ParseError)
 
 type Activity = (Person, ActivityName, [Event])
 
-type CSVResult = Either ParseError [Activity]
-
-type Person = String
-
-type ActivityName = String
-
-type Date = String
-
-type Event = (Date, String)
+type CSVResult = Either ParseError ParsedCSV
 
 fetchAndParse :: String -> IO CSVResult
 fetchAndParse url = parser <$> fetchUrl url
@@ -32,16 +23,25 @@ processRows parser s = pure process <*> parsed
   where
     parsed = parser s
 
-process :: CSV -> [Activity]
-process rows = processRow . addDates <$> [r | r <- tail rows, not (null (head r))]
-  where
-    addDates = zip (head rows)
+type ParsedCSV = [Activity]
 
-processRow :: [(Field, Field)] -> Activity
-processRow ((_, whom):(_, wat):acts) = (whom, wat, [a | a <- acts, not (null (snd a))])
+process :: CSV -> ParsedCSV
+process rows =
+  [(p, a, completedEvents es) | (p:a:es) <- tail rows, not (null p)]
+  where
+    completedEvents es = [e | e <- zip dates es, not (null (snd e))]
+    dates = drop 2 (head rows)
 
 fetchUrl :: String -> IO String
 fetchUrl url = rBody <$> get url
 
 rBody :: Response Char8.ByteString -> String
 rBody r = Char8.unpack $ r ^. responseBody
+
+type Person = String
+
+type ActivityName = String
+
+type Date = String
+
+type Event = (Date, String)
