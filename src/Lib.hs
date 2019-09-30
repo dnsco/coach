b74d1ff247ec83ac7e6c94e6c798ac4996d496dc
Dennis Collective <dennis.collective@gmail.com>
2019-09-30 00:57:52 -0700

    Filter out empty rows

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,21 +1,52 @@
 module Lib
   ( fetchAndParse
+  , processRows
   ) where
 
-import           Control.Arrow              (left)
 import           Control.Lens
-import           Data.ByteString.Lazy.Char8 as Char8
+import qualified Data.ByteString.Lazy.Char8 as Char8 (ByteString, unpack)
+import           Data.Maybe                 (catMaybes)
 import           Network.Wreq
 import           Text.CSV
+import           Text.Parsec.Error          (ParseError)
 
-fetchAndParse :: String -> IO (Either String CSV)
-fetchAndParse url = left show <$> result
+
+type Activity = (Person, ActivityName, [Event])
+
+type CSVResult = Either ParseError [Activity]
+
+type Person = String
+
+type ActivityName = String
+
+type Date = String
+
+type Event = (Date, String)
+
+fetchAndParse :: String -> IO CSVResult
+fetchAndParse url = parser <$> fetchUrl url
   where
-    parser = parseCSV url
-    result = parser <$> fetchUrl url
+    parser = processRows (parseCSV url)
+
+processRows :: (s -> Either ParseError CSV) -> s -> CSVResult
+processRows parser s = pure process <*> parsed
+  where
+    parsed = parser s
+
+process :: CSV -> [Activity]
+process rows = catMaybes (processRow <$> activities)
+  where
+    activities = zip (head rows) <$> tail rows
+
+processRow :: [(Field, Field)] -> Maybe Activity
+processRow ((_, whom):(_, wat):acts) =
+  if not (null whom)
+    then Just (whom, wat, [a | a <- acts, not (null (snd a))])
+    else Nothing
+processRow _ = Nothing
 
 fetchUrl :: String -> IO String
 fetchUrl url = rBody <$> get url
 
-rBody :: Response ByteString -> String
+rBody :: Response Char8.ByteString -> String
 rBody r = Char8.unpack $ r ^. responseBody
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE PartialTypeSignatures #-}
+
+import           Control.Arrow (right)
+import           Lib
+import           Test.Hspec
+import           Text.CSV
+
+fakeData = [["1"], ["2"], ["3"]]
+
+main =
+  hspec $ do
+    describe "FetchAndParse" $ do
+      it "is lit " $
+        ((length . head) <$> processRows fakeParser fakeData) `shouldBe`
+        (Right 3 :: Either _ Int)
+      it "is w lit " $
+        ((head . head) <$> processRows fakeParser fakeData) `shouldBe`
+        (Right ["1"] :: Either _ [String])
+      it "has fun" $ 2 + 2 `shouldBe` (4 :: Int)
+
+fakeParser :: CSV -> CSVResult
+fakeParser = Right
