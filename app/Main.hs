4bc5d7868e383bd147cca35833cb852c8a1a2d8f
Dennis Collective <dennis.collective@gmail.com>
2019-09-29 18:19:50 -0700

    switch to let notation so we can use do notation to figure out syntax

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import           Control.Applicative
 import           Control.Lens
 import           Data.ByteString.Lazy.Char8 as Char8
+import           Data.Either
 import           Lib
 import           Network.Wreq
 import           System.Environment
 import           Text.CSV
 
 main :: IO ()
-main = csv >>= print
-  where
-    url = errorIfBlank <$> (lookupEnv "SHEET_URL")
-    response = rBody <$> (url >>= get)
-    csv = parseCSV <$> url <*> response
+main =
+  let url = errorIfBlank <$> lookupEnv "SHEET_URL"
+      response = rBody <$> (url >>= get)
+      csv = parseCSV <$> url <*> response
+   in do c <- csv
+         print (fromRight [] c)
 
 errorIfBlank :: Maybe String -> String
 errorIfBlank Nothing  = error "set env SHEET_URL"
 errorIfBlank (Just s) = s
 
-rBody :: (Response ByteString) -> [Char]
+rBody :: Response ByteString -> String
 rBody r = Char8.unpack $ r ^. responseBody
