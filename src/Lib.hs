be0ee4ddaefd65d96cbfaf50ca32e1a066e2129f
Dennis Collective <dennis.collective@gmail.com>
2019-10-01 03:28:36 -0700

    backfill tests and format

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
   ( parseCsvAt
   , processRows
+  , ParsedCSV
+  , Activity
   ) where
 
 import           Control.Arrow              ((>>>))
@@ -13,31 +15,28 @@ import           Network.Wreq
 import           Text.CSV
 import           Text.Parsec.Error          (ParseError)
 
-type CSVResult = Either ParseError ParsedCSV
-
-parseCsvAt :: String -> IO CSVResult
-parseCsvAt url = parser <$> fetchUrl url
+parseCsvAt :: String -> IO (Either ParseError ParsedCSV)
+parseCsvAt url = fetchUrl url <&> parser
   where
-    parser = processRows (parseCSV url)
-
-processRows :: (s -> Either ParseError CSV) -> s -> CSVResult
-processRows parser s = pure process <*> parsed
-  where
-    parsed = parser s
+    parser s = processRows <$> parseCSV url s
 
 type Activity = (ActivityName, [Event])
 
 type ParsedCSV = Map Person [Activity]
 
-process :: CSV -> ParsedCSV
-process rows = Map.fromList [(p, [(a, completedEvents es)]) | (p:a:es) <- tail rows, not (null p)]
+processRows :: CSV -> ParsedCSV
+processRows rows =
+  Map.fromList
+    [(p, [(a, completedEvents es)]) | (p:a:es) <- tail rows, not (null p)]
   where
     completedEvents es = [e | e <- zip dates es, not (null (snd e))]
-    dates = [d | (Just d) <- dateCells <&> (splitOn "/" >>> map read >>> toDate)]
+    dates =
+      [d | (Just d) <- dateCells <&> (splitOn "/" >>> fmap read >>> toDate)]
     dateCells = drop 2 (head rows)
 
 toDate :: [Int] -> Maybe Date
-toDate [m, d, y] = Just Date {dateDay = d, dateMonth = toEnum (m - 1), dateYear = y}
+toDate [m, d, y] =
+  Just Date {dateDay = d, dateMonth = toEnum (m - 1), dateYear = y}
 toDate _ = Nothing
 
 fetchUrl :: String -> IO String
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,22 +1,35 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-
-import           Control.Arrow (right)
+import           Data.Char    (isSpace)
+import           Data.Functor ((<&>))
+import           Data.Map     as Map
+import           Data.Maybe   (fromJust)
 import           Lib
 import           Test.Hspec
-import           Text.CSV
+import           Text.CSV     as CSV (parseCSV)
 
-fakeData = [["1"], ["2"], ["3"]]
-
+--  let parsed = processRows <$> fakeData)
+--      ben = parsed <&> Map.lookup "ben"
+--   in
+main :: IO ()
 main =
   hspec $ do
     describe "FetchAndParse" $ do
-      it "is lit " $
-        ((length . head) <$> processRows fakeParser fakeData) `shouldBe`
-        (Right 3 :: Either _ Int)
-      it "is w lit " $
-        ((head . head) <$> processRows fakeParser fakeData) `shouldBe`
-        (Right ["1"] :: Either _ [String])
-      it "has fun" $ 2 + 2 `shouldBe` (4 :: Int)
+      it "seperates the people from the header" $ do
+        (csvData <&> length) `shouldBe` (Just 2 :: Maybe Int)
+        Map.size <$> processed `shouldBe` (Just 1 :: Maybe Int)
+      it "parse people's data" $ do
+        Map.member "ben" people' `shouldBe` True
+        fst (head (getPerson' "ben")) `shouldBe` "fishing"
+  where
+    rows =
+      [",,8/13/2087,8/14/2087,8/15/2087", "ben,fishing,x,x,THIIIISSSS BIGGGG"]
+    csv' = trim (unlines rows)
+    csvData = rightToMaybe (CSV.parseCSV "www.anunittest.com" csv')
+    processed = processRows <$> csvData
+    people' = fromJust processed
+    getPerson' s = fromJust (Map.lookup s people')
 
-fakeParser :: CSV -> CSVResult
-fakeParser = Right
+rightToMaybe :: Either a b -> Maybe b
+rightToMaybe = either (const Nothing) Just
+
+trim :: String -> String
+trim = (\f -> f . f) (reverse . dropWhile isSpace)
