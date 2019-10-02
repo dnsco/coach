131d318b6bc56559e8d27f981e6da76c512fd813
Dennis Collective <dennis.collective@gmail.com>
2019-10-01 18:17:10 -0700

    simplify tests a bit?

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,33 +1,27 @@
-import           Data.Char    (isSpace)
-import           Data.Functor ((<&>))
-import           Data.Map     as Map
-import           Data.Maybe   (fromJust)
+import           Data.Char  (isSpace)
+import           Data.Map   as Map
+import           Data.Maybe (fromJust)
 import           Lib
 import           Test.Hspec
-import           Text.CSV     as CSV (parseCSV)
 
---  let parsed = processRows <$> fakeData)
---      ben = parsed <&> Map.lookup "ben"
---   in
 main :: IO ()
 main =
   hspec $ do
+    let csvResult = rightToMaybe (parseAndProcess "/test/file/fake.csv" csv)
     describe "Paring a csv" $ do
-      let csv = trim (unlines rows)
-          csvData = rightToMaybe (CSV.parseCSV "www.anunittest.com" csv)
-          processed = rightToMaybe (parseAndProcess "/test/file/fake.csv" csv)
       it "seperates the people from the header" $ do
-        let csvRowCount = csvData <&> length
-            mapEntryCount = Map.size <$> processed
-        (>) <$> csvRowCount <*> mapEntryCount `shouldBe` Just True
+        let csvRowCount = length rows
+            mapEntryCount = Map.size <$> csvResult
+        ((csvRowCount >) <$> mapEntryCount) `shouldBe` Just True
         mapEntryCount `shouldBe` (Just 2 :: Maybe Int)
       context "Formatting people" $ do
-        let people' = fromJust processed
+        let people' = fromJust csvResult
             getPerson' s = fromJust (Map.lookup s people')
         it "parse people's data" $ do
           Map.member "ben" people' `shouldBe` True
           fst (head (getPerson' "ben")) `shouldBe` "fishing"
   where
+    csv = trim (unlines rows)
     rows =
       [ ",,8/13/2087,8/14/2087,8/15/2087"
       , "ben,xylophon,x,,8Mile"
