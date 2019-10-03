34662611a5ad17fe865a3cbe985b382ef1205efc
Dennis Collective <dennis.collective@gmail.com>
2019-10-02 23:11:18 -0700

    WEB FRAMEWORK YOLO

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,16 @@
 import           Data.Hourglass
+import           Data.Monoid        (mconcat)
+import           Data.String        (fromString)
+import           Data.Text.Lazy     (Text)
 import           Lib
 import           System.Environment
 import           System.Hourglass
+import           Web.Scotty
 
 main :: IO ()
 main = do
+  port <- getEnv "PORT"
+  sheetUrl <- getEnv "SHEET_URL"
   currentDate <- dtDate . localTimeUnwrap <$> localDateCurrent
-  people <- parseCsvAt =<< getEnv "SHEET_URL"
-  print (delinquents currentDate <$> people)
+  people <- parseCsvAt sheetUrl
+  scotty (read port) $ do get "/" $ do html $ mconcat [fromString (show (delinquents currentDate <$> people)) :: Text]
