dae053339566e3bfcbd42d4f5faef99d1e0cb26e
Dennis Collective <dennis.collective@gmail.com>
2019-10-02 19:12:41 -0700

    fetch for current day

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,11 @@
-import           Coach.Util         (parseDate)
-import           Data.Maybe         (fromJust)
+
+import           Data.Hourglass
 import           Lib
 import           System.Environment
+import           System.Hourglass
 
 main :: IO ()
 main = do
-  p <- parsed
-  print (delinquents (fromJust (parseDate "10/2/2019")) <$> p)
-  where
-    url = errorIfBlank <$> lookupEnv "SHEET_URL"
-    parsed = url >>= parseCsvAt
-
-errorIfBlank :: Maybe String -> String
-errorIfBlank Nothing  = error "set env SHEET_URL"
-errorIfBlank (Just s) = s
+  currentDate <- dtDate . localTimeUnwrap <$> localDateCurrent
+  people <- parseCsvAt =<< getEnv "SHEET_URL"
+  print (delinquents currentDate <$> people)
