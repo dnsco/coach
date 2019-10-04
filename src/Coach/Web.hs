e91ca8d0e8b381bfcb191c3a7902502d19f70064
Dennis Collective <dennis.collective@gmail.com>
2019-10-03 21:30:23 -0700

    link to sheet

--- src/Coach/Web.hs
+++ src/Coach/Web.hs
@@ -8,5 +8,10 @@ dqsResponse t (Right p) = showDqs $ delinquents t p
 dqsResponse _ (Left e)  = "FAILBOAT: " ++ show e
 
 showDqs :: Delinquents -> String
-showDqs []  = "EVERYBODY'S KOSHER"
-showDqs dqs = "Have these bitches stepped up? " ++ show dqs
+showDqs [] = "EVERYBODY'S KOSHER"
+showDqs dqs =
+  "Have these bitches stepped up? " ++
+  show dqs ++
+  "<br/> <br/>" ++
+  "<a href='https://docs.google.com/spreadsheets/d/1nbqOF_xE_ANFzA-pxnDVVG2iH45E6khYVHrc36l5Opo/edit#gid=0'>" ++
+  "GET IT!</a>"
