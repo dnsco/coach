980dae35a2445d10c20bc1472048d8d0a3e252f0
Dennis Collective <dennis.collective@gmail.com>
2019-10-05 14:56:55 -0700

    fix test and intellij

---
+++ frontend/__tests__/components/hello_test.tsx
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+import React from 'react'
+import {shallow} from 'enzyme'
+import {Hello} from "../../src/components/hello";
+
+describe('NiceCheckbox', () => {
+    it('renders the checkbox with correct label', () => {
+        expect(
+            shallow(
+                <Hello/>
+            ).text()
+        ).toEqual('YO')
+    })
+})
--- frontend/src/components/hello.tsx
+++ frontend/src/components/hello.tsx
@@ -1,1 +1,5 @@
-export default () => "YO"
+import React from "react";
+import {Fragment, FunctionComponent} from "react";
+
+const Hello: FunctionComponent<{}> = () => <Fragment>YO</Fragment>
+export {Hello}
