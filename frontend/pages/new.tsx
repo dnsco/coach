e27c5bf6b136197cce96d42d146e2146e48e17a1
Dennis Collective <dennis.collective@gmail.com>
2019-10-07 23:07:10 -0700

    Stylsheetslol

--- frontend/pages/new.tsx
+++ frontend/pages/new.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import { fetchNewPeople, IApiNewPerson } from "../src/api";
 
@@ -8,11 +9,29 @@ interface IProps {
 const NewPage = ({ people: [names, activities, ...days] }: IProps) => {
   return (
     <div>
+      <Head>
+        <title>Bish didyou?!?!!</title>
+        <link
+          rel="stylesheet"
+          href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
+        />
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css"
+        />
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css"
+        />
+      </Head>
       <table>
         <tr>
           {names.map((name, i) => (
-            <th key={name}>
-              {name} {activities[i]}
+            <th key={name} style={{ padding: "15px 20px" }}>
+              <h4 style={{ margin: "0px" }}>{name}</h4>{" "}
+              <h5 style={{ margin: "0px", fontWeight: "bold" }}>
+                {activities[i]}
+              </h5>
             </th>
           ))}
         </tr>
@@ -20,13 +39,18 @@ const NewPage = ({ people: [names, activities, ...days] }: IProps) => {
         {days.map((events, i) => (
           <tr key={i}>
             {events.map((e, j) => (
-              <td key={j}>{e}</td>
+              <td key={j} style={{ padding: "10px 20px" }}>
+                {e}
+              </td>
             ))}
           </tr>
         ))}
       </table>
 
-      <a href="https://docs.google.com/spreadsheets/d/1nbqOF_xE_ANFzA-pxnDVVG2iH45E6khYVHrc36l5Opo/edit#gid=0">
+      <a
+        href="https://docs.google.com/spreadsheets/d/1nbqOF_xE_ANFzA-pxnDVVG2iH45E6khYVHrc36l5Opo/edit#gid=0"
+        style={{ float: "right", paddingRight: "50px" }}
+      >
         Get it!
       </a>
     </div>
