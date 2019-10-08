ed30837d550a1b701436b83f92f401432f2daa1f
Dennis Collective <dennis.collective@gmail.com>
2019-10-07 23:12:44 -0700

    promote new page even though code is badlol

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,36 +1,72 @@
+import Head from "next/head";
 import React from "react";
-import { fetchDelinquents, IApiPerson } from "../src/api";
+import { fetchNewPeople, IApiNewPerson } from "../src/api";
 
 interface IProps {
-  people: IApiPerson[];
+  people: IApiNewPerson[];
 }
 
-const IndexPage = ({ people }: IProps) => {
+const NewPage = ({ people: [names, activities, ...days] }: IProps) => {
   return (
     <div>
-      {people.map(({ name, activities }) => (
-        <div key={name}>
-          <h3>{name}</h3>
-          {activities.map(({ title, isDelinquent }) => (
-            <span key={name + title}>
-              <h4 style={{ display: "inline" }}>{title}</h4>{" "}
-              {isDelinquent ? "Step it up, queen." : "You Got it!"}
-            </span>
+      <Head>
+        <title>Bish didyou?!?!!</title>
+        <link
+          rel="stylesheet"
+          href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
+        />
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css"
+        />
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css"
+        />
+      </Head>
+      <table>
+        <tr>
+          {names.map((name, i) => (
+            <th key={name} style={{ padding: "15px 20px" }}>
+              <h4 style={{ margin: "0px" }}>{name}</h4>{" "}
+              <h5 style={{ margin: "0px", fontWeight: "bold" }}>
+                {activities[i]}
+              </h5>
+            </th>
           ))}
-        </div>
-      ))}
-      <br />
-      <br />
-      <a href="https://docs.google.com/spreadsheets/d/1nbqOF_xE_ANFzA-pxnDVVG2iH45E6khYVHrc36l5Opo/edit#gid=0">
+        </tr>
+
+        {days.map((events, i) => (
+          <tr key={i}>
+            {events.map((e, j) => (
+              <td key={j} style={{ padding: "10px 20px" }}>
+                {e}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </table>
+
+      <a
+        href="https://docs.google.com/spreadsheets/d/1nbqOF_xE_ANFzA-pxnDVVG2iH45E6khYVHrc36l5Opo/edit#gid=0"
+        style={{ float: "right", marginRight: "50px" }}
+        className="button"
+      >
         Get it!
       </a>
     </div>
   );
 };
 
-IndexPage.getInitialProps = async (): Promise<IProps> => {
-  const people = await fetchDelinquents();
+// <span key={name + title}>
+//   <h4 style={{display: "inline"}}>{title}</h4>{" "}
+// {isDelinquent ? "Step it up, queen." : "You Got it!"}
+// </span>
+//
+
+NewPage.getInitialProps = async (): Promise<IProps> => {
+  const people = await fetchNewPeople();
   return { people };
 };
 
-export default IndexPage;
+export default NewPage;
