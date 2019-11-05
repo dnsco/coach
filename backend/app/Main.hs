bfca9c1cf962a7717bca7174e91413df212055bf
Dennis Collective <dennis.collective@gmail.com>
2019-11-04 17:21:20 -0800

    restructure binaries to not depend on each other

--- backend/app/Main.hs
+++
@@ -1,8 +0,0 @@
-import           Coach.Api                (coachApi)
-import           Network.Wai.Handler.Warp
-import           System.Environment
-
-main :: IO ()
-main = do
-  port <- getEnv "PORT"
-  run (read port) coachApi
--- backend/app/SendMessage.hs
+++
@@ -1,6 +0,0 @@
-module SendMessage where
-
-import           Coach.Messaging (getTwilioEnv, sendMessage)
-
-main :: IO ()
-main = sendMessage getTwilioEnv "WAT" >>= print
